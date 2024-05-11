2ba0f8eb0e2b4b12ef224fd35003b4cab5ef813b
Khoa <khoadha9a2.dt@gmail.com>
2024-05-11 10:08:26 +0700

    add swagger

--- Hosteland/Controllers/WeatherForecastController.cs
+++ Hosteland/Controllers/WeatherForecastController.cs
@@ -15,7 +15,7 @@ namespace Hosteland.Controllers {
             _logger = logger;
         }
 
-        [HttpGet]
+        [HttpGet("GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get() {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
                 Date = DateTime.Now.AddDays(index),
