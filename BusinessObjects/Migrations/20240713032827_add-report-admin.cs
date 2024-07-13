e676b57a1da91d158cace0938b45cd00d6a97a7c
locpt1811 <locpt1811@gmail.com>
2024-07-13 10:28:47 +0700

    add-migration

---
+++ BusinessObjects/Migrations/20240713032827_add-report-admin.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObjects.Migrations
+{
+    public partial class addreportadmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Reply",
+                table: "Reports",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reply",
+                table: "Reports");
+        }
+    }
+}
